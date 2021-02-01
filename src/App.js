0baa8613ed08cd260915de40fc690e7a92259d27
Lawrence O. Caulker <locaulker@gmail.com>
2021-02-01 15:35:38 -0500

    added infinite query functionality

--- src/App.js
+++ src/App.js
@@ -9,40 +9,44 @@ const searchUrl = `https://api.unsplash.com/search/photos/`
 function App() {
   const [loading, setLoading] = useState(false)
   const [photos, setPhotos] = useState([])
+  const [page, setPage] = useState(1)
 
   const fetchImages = async () => {
     setLoading(true)
     let url
-    url = `${mainUrl}${clientID}`
+    const ulrPage = `&page=${page}`
+    url = `${mainUrl}${clientID}${ulrPage}`
     try {
       const response = await fetch(url)
       const data = await response.json()
-      setPhotos(data)
+      setPhotos(oldPhotos => {
+        return [...oldPhotos, ...data]
+      })
       setLoading(false)
     } catch (error) {
-      setLoading(false)
       console.log(error)
+      setLoading(false)
     }
   }
 
   useEffect(() => {
     fetchImages()
-  }, [])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page])
 
   useEffect(() => {
     const event = window.addEventListener('scroll', () => {
-      // console.log(`innerHeight ${window.innerHeight}`)
-      // console.log(`scrollY ${window.iscrollY}`)
-      // console.log(`body height ${document.body.scrollHeight}`)
       if (
         !loading &&
         window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
       ) {
-        console.log('it worked')
+        setPage(oldPage => {
+          return oldPage + 1
+        })
       }
     })
     return () => window.removeEventListener('scroll', event)
-  }, [])
+  }, [loading])
 
   const handleSubmit = e => {
     e.preventDefault()
